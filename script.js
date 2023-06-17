7fd3ae29a731e91709b79ad3bf4ab1733701951d
Anita Cambon <anita_cambon@hotmail.com>
2023-06-16 21:56:38 -0300

    falta trabajar sobre find, sort y reduce

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let competidores = [
-    { nombre: "Diaz, Nicolas", edad: "48 - Master 3", faixa: "negro", categoria: "medio pesado", medallas: "1" },
-    { nombre: "Gomez, Julian", edad: "37 - Master 2", faixa: "negro", categoria: "pesado", medallas: "3" },
-    { nombre: "Cambon, Ana", edad: "32 - Master 1", faixa: "blanco", categoria: "pena", medallas: "1" },
-    { nombre: "Rodriguez, Julieta", edad: "Adulto 1", faixa: "violeta", categoria: "leve", medallas: "2" },
-    { nombre: "Roca, Leandro", edad: "Adulto 1", faixa: "marron", categoria: "pena", medallas: "0" },
-    { nombre: "Fernandez, Matias", edad: "Master 4", faixa: "blanco", categoria: "medio pesado", medallas: "3" },
-    { nombre: "Correa, Marcela", edad: "Adulto 2", faixa: "marron", categoria: "pena", medallas: "1" },
-    { nombre: "Acosta, Jimena", edad: "Master 1", faixa: "azul", categoria: "leve", medallas: "2" },
-    { nombre: "Lopez, Mario", edad: "Master 3", faixa: "azul", categoria: "pesado", medallas: "4" },
-    { nombre: "Martinez, Gonzalo", edad: "Master 2", faixa: "marron", categoria: "pena", medallas: "1" }
+    { nombre: "Diaz, Nicolas", edad: "48", faixa: "negro", categoria: "medio pesado", medallas: 1 },
+    { nombre: "Gomez, Julian", edad: "37", faixa: "negro", categoria: "pesado", medallas: 3 },
+    { nombre: "Cambon, Ana", edad: 32, faixa: "blanco", categoria: "pena", medallas: 1 },
+    { nombre: "Rodriguez, Julieta", edad: 22, faixa: "violeta", categoria: "leve", medallas: 2 },
+    { nombre: "Roca, Leandro", edad: 24, faixa: "marron", categoria: "pena", medallas: 0 },
+    { nombre: "Fernandez, Matias", edad: 47, faixa: "blanco", categoria: "medio pesado", medallas: 3 },
+    { nombre: "Correa, Marcela", edad: 26, faixa: "marron", categoria: "pena", medallas: 1 },
+    { nombre: "Acosta, Jimena", edad: 31, faixa: "azul", categoria: "leve", medallas: 2 },
+    { nombre: "Lopez, Mario", edad: 44, faixa: "azul", categoria: "pesado", medallas: 4 },
+    { nombre: "Martinez, Gonzalo", edad: 40, faixa: "marron", categoria: "pena", medallas: 1 }
 ]
 
-let mensaje = "Seleccione una opción:\n1 - Listado de competidores\n2 - Filtrar por faixa\n3 - Ordenar por edad\n4 - Buscar por palabra clave\n5 - Total de medallas ganadas\n0 - SALIR"
+let mensaje = "Seleccione una opción:\n1 - Listado de competidores\n2 - Filtrar por faixa\n3 - Ordenar por edad menor a mayor\n4 - Buscar cuantás medallas ganó un competidor\n5 - Total de medallas ganadas\n0 - SALIR"
 
 let opcion
 
@@ -19,18 +19,22 @@ do {
     opcion = Number(prompt(mensaje))
     if (opcion === 1) {
         listar(competidores)
-    } if (opcion === 2) {
-        let faixa = prompt("Elegir categoria: blanco, azul, violeta, marron, negro")
+    } else if (opcion === 2) {
+        let faixa = prompt("Elegir faixa: blanco, azul, violeta, marron, negro")
         let competidoresFiltrados = competidores.filter(competidor => competidor.faixa === faixa)
         alert(listar(competidoresFiltrados))
-    } if (opcion === 3) {
-
-    } if (opcion === 4) {
-        let palabraClave = prompt("Ingrese palabra clave")
-        let competidoresBuscados = competidores.find(competidor => competidor.nombre || competidor.edad || competidor.faixa || competidor.categoria === palabraClave)
-        alert(listar(competidoresBuscados))
+    } else if (opcion === 3) {
+        /* ordenar(competidores)*/
+    } else if (opcion === 4) {
+        /* let cantidad = prompt("Ingrese un número de 0 a 4 inclusive")
+        let medallasSeleccionadas = competidores.find(competidor => competidor.medallas === cantidad)
+        alert(medallasSeleccionadas)*/
+    } else if (opcion === 5) {
     }
-} while (opcion !== 0)
+} while (opcion !== 6) {
+    alert("Gracias por su consulta")
+}
+
 
 function listar(arrayAListar) {
     let listado = " "
@@ -39,4 +43,23 @@ function listar(arrayAListar) {
     })
     alert(listado)
 }
+
+/* 
+function ordenar(arrayAOrdenar) {
+    return arrayAOrdenar.sort((a, b) => {
+        if (a.edad < b.edad) {
+            return -1
+        }
+        if (a.edad > b.edad) {
+            return 1
+        }
+        return 0
+    })
+}
+
+function sumar(arrayTotalMedallas) {
+
+}
+*/
+
 
