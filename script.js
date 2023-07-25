db72d402517adef0b6eae7a59534f8147d0955f1
Anita Cambon <anita_cambon@hotmail.com>
2023-07-25 00:39:14 -0300

    primeros avances con fetch

--- script.js
+++ script.js
@@ -1,45 +1,12 @@
-function catalogoActualizado(saleTodoBien) {
-    let productos = [
-        { id: 1001, nombre: "Lycra Julia Pareja", categoria: "indumentaria", talle: "S", unidades: 500, precio: 5000, rutaImagen: "lycra_julia.jpeg" },
-        { id: 1002, nombre: "Lycra Julia Pareja", categoria: "indumentaria", talle: "M", unidades: 500, precio: 5000, rutaImagen: "lycra_julia.jpeg" },
-        { id: 1003, nombre: "Lycra Julia Pareja", categoria: "indumentaria", talle: "L", unidades: 500, precio: 5000, rutaImagen: "lycra_julia.jpeg" },
-        { id: 2001, nombre: "Lycra Le Fosse", categoria: "indumentaria", talle: "S", unidades: 500, precio: 5000, rutaImagen: "lycra_lean.jpeg" },
-        { id: 2002, nombre: "Lycra Le Fosse", categoria: "indumentaria", talle: "M", unidades: 500, precio: 5000, rutaImagen: "lycra_lean.jpeg" },
-        { id: 2003, nombre: "Lycra Le Fosse", categoria: "indumentaria", talle: "L", unidades: 500, precio: 5000, rutaImagen: "lycra_lean.jpeg" },
-        { id: 3001, nombre: "Lycra Magna 2020", categoria: "indumentaria", talle: "S", unidades: 500, precio: 4000, rutaImagen: "lycra2020.jpeg" },
-        { id: 3002, nombre: "Lycra Magna 2020", categoria: "indumentaria", talle: "M", unidades: 500, precio: 4000, rutaImagen: "lycra2020.jpeg" },
-        { id: 3003, nombre: "Lycra Magna 2020", categoria: "indumentaria", talle: "L", unidades: 500, precio: 4000, rutaImagen: "lycra2020.jpeg" },
-        { id: 4001, nombre: "Musculosa Boxing", categoria: "indumentaria", talle: "S", unidades: 500, precio: 3500, rutaImagen: "musc_box.jpeg" },
-        { id: 4002, nombre: "Musculosa Boxing", categoria: "indumentaria", talle: "M", unidades: 500, precio: 3500, rutaImagen: "musc_box.jpeg" },
-        { id: 4003, nombre: "Musculosa Boxing", categoria: "indumentaria", talle: "L", unidades: 500, precio: 3500, rutaImagen: "musc_box.jpeg" },
-        { id: 5001, nombre: "Parche Magna", categoria: "accesorios", unidades: 500, precio: 1000, rutaImagen: "parche.jpeg" },
-        { id: 6001, nombre: "Kit Kick Boxing", categoria: "accesorios", unidades: 500, precio: 7000, rutaImagen: "kit_kb.jpeg" },
-        { id: 7001, nombre: "Kimono Negro", categoria: "indumentaria", talle: "A0", unidades: 500, precio: 20000, rutaImagen: "gi_negro.jpeg" },
-        { id: 7002, nombre: "Kimono Negro", categoria: "indumentaria", talle: "A1", unidades: 500, precio: 20000, rutaImagen: "gi_negro.jpeg" },
-        { id: 7003, nombre: "Kimono Negro", categoria: "indumentaria", talle: "A2", unidades: 500, precio: 20000, rutaImagen: "gi_negro.jpeg" },
-        { id: 7004, nombre: "Kimono Negro", categoria: "indumentaria", talle: "A3", unidades: 500, precio: 20000, rutaImagen: "gi_negro.jpeg" },
-        { id: 8001, nombre: "Kimono Azul", categoria: "indumentaria", talle: "A0", unidades: 500, precio: 20000, rutaImagen: "gi_azul.jpeg" },
-        { id: 8002, nombre: "Kimono Azul", categoria: "indumentaria", talle: "A1", unidades: 500, precio: 20000, rutaImagen: "gi_azul.jpeg" },
-        { id: 8003, nombre: "Kimono Azul", categoria: "indumentaria", talle: "A2", unidades: 500, precio: 20000, rutaImagen: "gi_azul.jpeg" },
-        { id: 8004, nombre: "Kimono Azul", categoria: "indumentaria", talle: "A3", unidades: 500, precio: 20000, rutaImagen: "gi_azul.jpeg" },
-        { id: 9501, nombre: "Gorro lana", categoria: "accesorios", unidades: 500, precio: 5000, rutaImagen: "gorro_lana.jpeg" },
-        { id: 11001, nombre: "Evento MMA", categoria: "eventos", unidades: 500, precio: 6000, rutaImagen: "evento_mma.jpeg" },
-        { id: 11000, nombre: "Seminario Leandro", categoria: "eventos", unidades: 500, precio: 2500, rutaImagen: "evento_seminario.jpeg" },
-        { id: 12002, nombre: "Pase 8 clases", categoria: "cuota", unidades: 500, precio: 4500, rutaImagen: "logo.jpeg" },
-        { id: 12003, nombre: "Pase 12 clases", categoria: "cuota", unidades: 500, precio: 6000, rutaImagen: "logo.jpeg" },
-        { id: 12004, nombre: "Pase libre", categoria: "cuota", unidades: 500, precio: 10500, rutaImagen: "logo.jpeg" }
-    ]
+const urlLocal = '../productos.json'
+let productos = []
 
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            saleTodoBien ? resolve(productos) : reject("Algo salió mal")
-        }, 0)
+fetch(urlLocal)
+    .then(response => response.json())
+    .then(contenedor => {
+        productos.push(contenedor)
+        renderizar(productos, contenedor, carrito)
     })
-}
-
-catalogoActualizado(true)
-    .then(respuesta => catalogoPrincipal(respuesta))
     .catch(mensajeError => alert(mensajeError))
 
 
@@ -62,8 +29,8 @@ function catalogoPrincipal(productos) {
 
     let carrito = JSON.parse(localStorage.getItem("carrito")) || ([])
 
-    crearCategorias(productos, contenedorFiltros)
     renderizar(productos, contenedor, carrito)
+    crearCategorias(productos, contenedorFiltros)
 }
 
 
@@ -128,13 +95,10 @@ function renderizar(productos, contenedor, carrito) {
     })
 }
 
-function agregarAlCarrito(productos, id, carrito) {
-    let productoBuscado = productos.find(producto => producto.id === id)
-    let posicionProductoEnCarrito = carrito.findIndex(producto => producto.id === id)
+function agregarAlCarrito(e) {
+    let productoBuscado = productos.find(producto => producto.id === e.currentTarget.id)
+    let posicionProductoEnCarrito = carrito.findIndex(producto => producto.id === productoBuscado.id)
     if (posicionProductoEnCarrito !== -1) {
-        carrito[posicionProductoEnCarrito].unidades++
-        carrito[posicionProductoEnCarrito].subtotal = carrito[posicionProductoEnCarrito].unidades * carrito[posicionProductoEnCarrito].precioUnitario
-    } else {
         carrito.push({
             id: productoBuscado.id,
             nombre: productoBuscado.nombre,
@@ -142,6 +106,9 @@ function agregarAlCarrito(productos, id, carrito) {
             unidades: 1,
             subtotal: productoBuscado.precio
         })
+    } else {
+        carrito[posicionProductoEnCarrito].unidades++
+        carrito[posicionProductoEnCarrito].subtotal = carrito[posicionProductoEnCarrito].unidades * carrito[posicionProductoEnCarrito].precioUnitario
     }
     lanzarTostada()
     localStorage.setItem("carrito", JSON.stringify(carrito))
@@ -158,26 +125,30 @@ function lanzarTostada() {
 
 
 function renderizarCarrito(carrito) {
+    let total = 0
+    let totalProductos = 0
     let carritoFisico = document.getElementById("carrito")
     carritoFisico.classList.add("claseDelCarrito")
     carritoFisico.innerHTML = ""
+
+    let listaCarrito = document.createElement("ul")
+    listaCarrito.classList.add("cart-list")
+
     carrito.forEach(({ id, nombre, precioUnitario, unidades, subtotal }) => {
-        let elementoDelCarrito = document.createElement("div")
-        elementoDelCarrito.innerHTML = `
-        <p>ID: ${id} | Producto: ${nombre} | Precio: $${precioUnitario} | Cantidad: ${unidades} | Subtotal: $${subtotal} <button type="button" class="bi bi-trash3"></button></p>\n`
+        let elementoDelCarrito = document.createElement("li")
+        elementoDelCarrito.classList.add("cart-item")
+        elementoDelCarrito.innerHTML += `
+            <p>Producto: ${nombre} | Precio: $${precioUnitario} | Cantidad: ${unidades} | Subtotal: $${subtotal}</p>
+        `
+        listaCarrito.appendChild(elementoDelCarrito)
         carritoFisico.appendChild(elementoDelCarrito)
+        total += subtotal
+        totalProductos += unidades
     })
+    carritoFisico.innerHTML += `
+    <li class= "totalCarrito">Total $${total}</li>`
 }
-
-/*
-function calcularSumaTotal(carritoFisico) {
-    let suma = 0
-    for (let i = 0; i < carritoFisico.length; i++) {
-        suma += carritoFisico[i].subtotal
-    }suma(subtotal)
-}*/
-
-
+/*<button ${id} type="button" class="bi bi-trash3"></button>*/
 
 function mostrarOcultar() {
     let padreContenedor = document.getElementById("productosContenedor")
@@ -193,3 +164,4 @@ function finalizarCompra(carritoJSON) {
     localStorage.removeItem("carrito")
     carritoJSON = ([])
 }
+
